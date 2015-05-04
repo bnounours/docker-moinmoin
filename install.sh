d7a4022d065826d8fc4d7aaa64e2d923958cfec9
b_nounours <b_nounours@hotmail.com>
2015-05-04 23:26:18 +0200

    Installation OK, not yet customization of path / mail server / backup

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+WORKDIR=/opt
+
+
+if [ -z "$1" ];then
+  echo "Usage : $0 <MoinMoin Version"
+  echo "Exiting ..."
+  exit 1
+fi
+
+MM_VERSION=$1
+
+echo "===== [$(date +%F)] MoinMoin ($MM_VERSION) Install Script =====" | tee -a $WORKDIR/install.log
+
+echo "Installing necessary packages ..." | tee -a $WORKDIR/install.log
+apt-get -y install python wget
+
+
+echo "Downloading Moinmoin package" | tee -a $WORKDIR/install.log
+cd /tmp
+wget http://static.moinmo.in/files/moin-$MM_VERSION.tar.gz
+if [ $? -neq 0 ]; then
+  echo "Error while downloading MoinMoin $MM_VERSION from http://static.moinmo.in/files/moin-$MM_VERSION.tar.gz" | tee -a $WORKDIR/install.log
+  exit 1
+fi
+
+echo "Untar archive to $WORKDIR" | tee -a $WORKDIR/install.log
+tar -xvzf moin-$MM_VERSION.tar.gz -C $WORKDIR
+
+echo "Modification of conf to listen on 0.0.0.0" | tee -a $WORKDIR/install.log
+cd $WORKDIR/moin-$MM_VERSION;
+sed -i".ori" "s/hostname = 'localhost'/hostname = '0.0.0.0'/g" wikiserverconfig.py
+
+echo "Create user moin" | tee -a $WORKDIR/install.log
+useradd -Mr moin
+
+echo "Change rights..." | tee -a $WORKDIR/install.log
+chown moin. /opt/moin-$MM_VERSION/wiki/data /opt/moin-$MM_VERSION/wiki/data/pages /opt/moin-$MM_VERSION/wiki/underlay /opt/moin-$MM_VERSION/wiki/underlay/pages
+chown moin. -R /opt/moin-$MM_VERSION/wiki/data/pages /opt/moin-$MM_VERSION/wiki/underlay/pages
+chmod 750 /opt/moin-$MM_VERSION/wiki/data /opt/moin-$MM_VERSION/wiki/data/pages
+
+echo "Writing Supervisor Conf" | tee -a $WORKDIR/install.log 
+echo "[program:moinmoin]\ncommand=/usr/bin/python /opt/moin-$MM_VERSION/wikiserver.py\nuser=moin\nredirect_stderr=true" > /etc/supervisor/conf.d/moin.conf 
+
